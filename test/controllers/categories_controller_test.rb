aff3e4f04363a00791fb45a84fc8b3abe09fd5a8
Bryan Hall <hallbryand@gmail.com>
2018-03-13 20:24:12 +0000

    Fix tests and update navbar with categories

--- test/controllers/categories_controller_test.rb
+++ test/controllers/categories_controller_test.rb
@@ -3,6 +3,7 @@ require 'test_helper'
 class CategoriesControllerTest < ActionController::TestCase
     def setup
         @category = Category.create(name: "sports")
+        @user = User.create(username: "Hammy", email: "a@a.com", password: "asdf", admin: true)
     end
     
     test "should get categories index" do
@@ -11,6 +12,7 @@ class CategoriesControllerTest < ActionController::TestCase
     end
     
     test "should get new" do
+        session[:user_id] = @user.id
         get :new
         assert_response :success
     end
@@ -19,4 +21,11 @@ class CategoriesControllerTest < ActionController::TestCase
         get(:show, { 'id' => @category.id })
         assert_response :success
     end
+    test "should redirect create when admin not logged in" do
+        assert_no_difference 'Category.count' do
+            post :create, category: { name: "Sports" }
+        end
+        assert_redirected_to categories_path
+    end
+    
 end
--- test/integration/create_categories_test.rb
+++ test/integration/create_categories_test.rb
@@ -1,6 +1,10 @@
 require 'test_helper'
 
 class CreateCategoriesTest < ActionDispatch::IntegrationTest
+    def setup
+        @user = User.create(username: "Hammy", email: "a@a.com", password: "asdf", admin: true)
+    end
+    
     test "get new category form and create category" do
         get new_category_path
         assert_template 'categories/new'
@@ -12,6 +16,7 @@ class CreateCategoriesTest < ActionDispatch::IntegrationTest
     end
     
     test "invalid category submission results in failure" do
+        sign_in_as(@user, "asdf")
         get new_category_path
         assert_template 'categories/new'
         assert_no_difference 'Category.count' do
